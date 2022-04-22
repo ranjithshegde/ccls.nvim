7ad33a593466521aedeaf89cf0443f9e9b032fd3
ranjithshegde <ranjithshegde@gmail.com>
2022-04-22 09:18:17 +0200

    Initial commit
    add self mehtod

---
+++ ftplugin/NodeTree.lua
@@ -0,0 +1,24 @@
+if vim.b.did_ftplugin then
+    return
+else
+    vim.b.did_ftplugin = 1
+
+    vim.opt_local.bufhidden = "wipe"
+    vim.opt_local.buftype = "nofile"
+    vim.opt_local.foldcolumn = "0"
+    vim.opt_local.foldmethod = "manual"
+    vim.opt_local.buflisted = false
+    vim.opt_local.foldenable = false
+    vim.opt_local.list = false
+    vim.opt_local.modifiable = false
+    vim.opt_local.number = false
+    vim.opt_local.spell = false
+    vim.opt_local.swapfile = false
+    vim.opt_local.wrap = false
+
+    if vim.fn.exists(vim.g.nodetree_no_default_maps) ~= 1 then
+        vim.keymap.set("n", "o", "<Plug>(nodetree-toggle-node)", { buffer = true })
+        vim.keymap.set("n", "<cr>", "<Plug>(nodetree-execute-node)", { buffer = true })
+        vim.keymap.set("n", "q", "<Plug>(nodetree-wipe-tree)", { buffer = true })
+    end
+end
---
+++ lua/ccls/init.lua
@@ -0,0 +1,33 @@
+local ccls = {}
+
+function ccls.setup() end
+
+function ccls.var(kind)
+    require("ccls.protocol").request("$ccls/vars", { kind = kind or 0 }, false)
+end
+
+function ccls.call(callee)
+    require("ccls.protocol").request("$ccls/call", { callee = callee or false }, false)
+end
+
+function ccls.callHeirarchy(callee)
+    require("ccls.protocol").request("$ccls/member", { callee = callee or false }, true)
+end
+
+function ccls.member(kind)
+    require("ccls.protocol").request("$ccls/member", { kind = kind or 4 }, false)
+end
+
+function ccls.memberHeirarchy(kind)
+    require("ccls.protocol").request("$ccls/member", { kind = kind or 4 }, true)
+end
+
+function ccls.inheritance(derived)
+    require("ccls.protocol").request("$ccls/inheritance", { derived = derived or false }, false)
+end
+
+function ccls.inheritanceHeirarchy(derived)
+    require("ccls.protocol").request("$ccls/member", { kind = derived or false }, true)
+end
+
+return ccls
